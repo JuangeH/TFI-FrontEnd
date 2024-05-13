ce47447048d7892b4cb90c1ae1632d573b8af46f
Juan Hola <JuanH@usuario.com>
2024-05-12 23:46:32 -0300

    Se desactivan estilos básicos de Blazor y se agregan páginas.

--- InnoviaReach - TFI - FrontEnd/InnoviaReach - TFI - FrontEnd/wwwroot/js/ToolBar.js
+++ InnoviaReach - TFI - FrontEnd/InnoviaReach - TFI - FrontEnd/wwwroot/js/ToolBar.js
@@ -1,27 +1,23 @@
-﻿const expand_btn = document.querySelector(".expand-btn");
-
-let activeIndex;
-
-expand_btn.addEventListener("click", () => {
-    document.body.classList.toggle("collapsed");
-});
-
-const current = window.location.href;
-
-const allLinks = document.querySelectorAll(".sidebar-links a");
-
-allLinks.forEach((elem) => {
-    elem.addEventListener('click', function () {
-        const hrefLinkClick = elem.href;
-
-        allLinks.forEach((link) => {
-            if (link.href == hrefLinkClick) {
-                link.classList.add("active");
-            } else {
-                link.classList.remove('active');
-            }
+﻿function expandCollapseBody() {
+    const expand_btn = document.querySelector(".expand-btn");
+    if (expand_btn) {
+        expand_btn.addEventListener("click", () => {
+            document.body.classList.toggle("collapsed");
         });
-    })
-});
-
-
+    }
+}
+function activateLinks() {
+    const allLinks = document.querySelectorAll(".sidebar-links a");
+    allLinks.forEach((elem) => {
+        elem.addEventListener('click', function () {
+            const hrefLinkClick = elem.href;
+            allLinks.forEach((link) => {
+                if (link.href == hrefLinkClick) {
+                    link.classList.add("active");
+                } else {
+                    link.classList.remove('active');
+                }
+            });
+        });
+    });
+}
