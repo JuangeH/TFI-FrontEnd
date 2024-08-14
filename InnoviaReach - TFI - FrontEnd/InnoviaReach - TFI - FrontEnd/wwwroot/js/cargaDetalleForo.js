c36bdbb78da07e377fe89d67d729cba6c3afc974
Juan Hola <JuanH@usuario.com>
2024-08-13 23:52:22 -0300

    Se agregan mas cambios

---
+++ InnoviaReach - TFI - FrontEnd/InnoviaReach - TFI - FrontEnd/wwwroot/js/cargaDetalleForo.js
@@ -0,0 +1,31 @@
+﻿$('.newPost button[data-func]').click(function () {
+    document.execCommand($(this).data('func'), false);
+});
+
+$('.newPost select[data-func]').change(function () {
+    var $value = $(this).find(':selected').val();
+    document.execCommand($(this).data('func'), false, $value);
+});
+
+if (typeof (Storage) !== "undefined") {
+
+    $('.editor').keypress(function () {
+        $(this).find('.saved').detach();
+    });
+    $('.editor').html(localStorage.getItem("wysiwyg"));
+
+    $('button[data-func="save"]').click(function () {
+        $content = $('.editor').html();
+        localStorage.setItem("wysiwyg", $content);
+        $('.editor').append('<span class="saved"><i class="fa fa-check"></i></span>').fadeIn(function () {
+            $(this).find('.saved').fadeOut(500);
+        });
+    });
+
+    $('button[data-func="clear"]').click(function () {
+        $('.editor').html('');
+        localStorage.removeItem("wysiwyg");
+    });
+
+
+} 
