326522209ce5347fd149f020014d8cc33ad7f49e
Juan Hola <JuanH@usuario.com>
2024-08-17 18:50:28 -0300

    Se agrega pagina de catalogo

--- InnoviaReach - TFI - FrontEnd/InnoviaReach - TFI - FrontEnd/wwwroot/js/Recomendaciones/Recomendaciones.js
+++ InnoviaReach - TFI - FrontEnd/InnoviaReach - TFI - FrontEnd/wwwroot/js/Recomendaciones/Recomendaciones.js
@@ -48,40 +48,4 @@
             }
         ]
     });
-
-    // categorywise filtering
-    let buttonGroup = $('.button-group button');
-    let categoryItemsList = $('.category-item');
-    let initialActiveCategory = $($(buttonGroup)[0]).data('filter');
-
-    const setActiveButton = (categoryName) => {
-        jQuery.each(buttonGroup, function (index, buttonItem) {
-            if ($(buttonItem).data('filter') == categoryName) {
-                $(buttonItem).addClass('active-filter-button');
-            } else {
-                $(buttonItem).removeClass('active-filter-button');
-            }
-        })
-    }
-
-    const filterItems = (categoryName) => {
-        jQuery.each(categoryItemsList, function (index, categoryItem) {
-            if ($(categoryItem).hasClass(categoryName)) {
-                $(categoryItem).css('display', 'block');
-            } else {
-                $(categoryItem).css('display', 'none');
-            }
-        })
-    }
-
-    setActiveButton(initialActiveCategory);
-    filterItems(initialActiveCategory);
-
-    jQuery.each(buttonGroup, function (index, buttonItem) {
-        $(buttonItem).click(function () {
-            let categoryName = $(buttonItem).data('filter');
-            setActiveButton(categoryName);
-            filterItems(categoryName);
-        });
-    })
 });
